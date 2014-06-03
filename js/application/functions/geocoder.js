4b891760c748bebee706ac94bbd526a516079881
Jason Lieu <jlieu@esri.com>
2014-06-03 17:30:24 -0400

    Started GUI and tool bar

---
+++ js/application/functions/geocoder.js
@@ -0,0 +1,18 @@
+﻿require([
+    "esri/dijit/Geocoder"
+],
+function (
+    Geocoder
+    ) {
+    var geocoder = new Geocoder({
+        map: mapDiv,
+        autoComplete: true,
+        arcgisGeocoder: {
+            url: "http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer",
+            name: "Esri World Geocoder",
+            placeholder: "Find Address",
+            sourceCountry: "USA" // limit search to the United States
+        }
+    },"geocoder");
+    geocoder.startup();
+});
---
+++ js/application/mainApp.js
@@ -0,0 +1,20 @@
+﻿var map;
+require([
+    "esri/map",
+    "dojo/parser",
+    "dijit/layout/BorderContainer",
+    "dijit/layout/ContentPane",
+    "dojo/domReady!"
+],
+function (
+    Map,
+    parser,
+    BorderContainer,
+    ContentPane
+    ) {
+    map = new Map("mapDiv", {
+        center: [-56.049, 38.485],
+        zoom: 3,
+        basemap: "streets"
+    });
+});
