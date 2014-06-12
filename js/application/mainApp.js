7a3860ee05e7cce7709413f505d4043d0a0c8d95
Jason Lieu <jlieu@esri.com>
2014-06-12 16:01:32 -0400

    conflicts results tied to legend

--- js/application/mainApp.js
+++ js/application/mainApp.js
@@ -18,6 +18,9 @@ require([
     "esri/dijit/Geocoder",
     "dojo/fx",
     "dojo/fx/easing",
+    "dgrid/Grid",
+    "dijit/registry",
+
     "dijit/form/HorizontalSlider",
     "dijit/form/HorizontalRule",
     "dijit/form/HorizontalRuleLabels",
@@ -42,100 +45,78 @@ function (
     BasemapGallery,
     Geocoder,
     coreFx,
-    easing
+    easing,
+    Grid,
+    registry
     ) {
     //parser.parse();
+    //map content
     map = new Map("mapDiv", {
         center: [-80, 38.485],
         zoom: 6,
         basemap: "streets"
     });
 
-    //var legend = new LegendWidget({ map: map }, "legend");
-    //legend.startup();
+    //geocoder tool
+    var geocoder = new Geocoder({
+        arcgisGeocoder: {
+            placeholder: "find address"
+        },
+        autoComplete: true,
+        map: map,
+    }, 'search');
+    geocoder.startup();
 
+    //click legend tool
     on(dom.byId("legend"), "click", function (e) {
-        //var saveMyTabWidget = dom.byId('legendContainer');
-        //dom.byId('legendContainer').removeChild(dom.byId('legendContainer'));
+        var centerPane = registry.byId("centerPane");
+        var bottomPane = registry.byId("bottomPane");
 
-        //if (typeof saveMyTabWidget != "undefined") {
-        //    saveMyTabWidget.setHref(theURL);
-        //    dom.byId('legendContainer').addChild(saveMyTabWidget, 0);
-        //    saleProductsTabWidget = undefined;
-        //}
-        dojo.toggleClass("legendContainer", "style1");
-        dojo.toggleClass("centerPane", "style2");
-        //var display = domStyle.get(dom.byId("legendContainer"), "display");
-        //if (display == "none") {
-        //    domStyle.set(dom.byId("legendContainer"), "display", '');
-        //} else {
-        //    domStyle.set(dom.byId("legendContainer"), "display", 'none');
-        //}
-        map.resize();
-    });
+        if (domStyle.get(dom.byId("bottomPane"), "display") === "block") {
+            //console.log("shown");
 
-
-    var forOrganization = new Memory({
-        data: [
-            { name: "Alabama", id: "AL" },
-            { name: "Alaska", id: "AK" },
-            { name: "American Samoa", id: "AS" },
-            { name: "Arizona", id: "AZ" },
-            { name: "Arkansas", id: "AR" },
-            { name: "Armed Forces Europe", id: "AE" },
-            { name: "Armed Forces Pacific", id: "AP" },
-            { name: "Armed Forces the Americas", id: "AA" },
-            { name: "California", id: "CA" },
-            { name: "Colorado", id: "CO" },
-            { name: "Connecticut", id: "CT" },
-            { name: "Delaware", id: "DE" }
-        ]
-    });
-
-    var comboBox = new ComboBox({
-        id: "forOrg",
-        name: "state",
-        value: "California",
-        store: forOrganization,
-        searchAttr: "name"
-    }, "forOrg");
-
-    var comboBox = new ComboBox({
-        id: "forLayer",
-        name: "state",
-        value: "California",
-        store: forOrganization,
-        searchAttr: "name"
-    }, "forLayer");
-
-    var comboBox = new ComboBox({
-        id: "inOrg",
-        name: "state",
-        value: "California",
-        store: forOrganization,
-        searchAttr: "name"
-    }, "inOrg");
-
-    var comboBox = new ComboBox({
-        id: "inLayer",
-        name: "state",
-        value: "California",
-        store: forOrganization,
-        searchAttr: "name"
-    }, "inLayer");
-
-    var cancelBtn = new Button({
-        label: "Close",
-        onClick: function () {
-            console.log('clicked');
-            dijit.byId("conflictsDialog").hide();
+            coreFx.wipeOut({
+                node: "bottomPane",
+                duration: 800,
+                easing: easing.expoOut
+            }).play();
+            domStyle.set(dom.byId("bottomPane"), "display", "none");
+            centerPane.resize({ h: 900 });
+            bottomPane.resize({ h: 0 });
+        } else {
+            //console.log("none");
+            coreFx.wipeIn({
+                node: "bottomPane",
+                duration: 800,
+                easing: easing.expoOut
+            }).play();
+            domStyle.set(dom.byId("bottomPane"), "display", "block");
+            //registry.byId("bottomPane").resize({h: 400, w:400});
+            centerPane.resize({ h: 600 });
+            bottomPane.resize({ h: 0 });
         }
-    }, "cancel");
-
-    on(dom.byId("findConflicts"), "click", function (e) {
-        dijit.byId("conflictsDialog").show();
     });
 
+    //data grid    
+    var data = [
+        { first: "Bob", last: "Barker", age: 89 },
+        { first: "Vanna", last: "White", age: 55 },
+        { first: "Pat", last: "Sajak", age: 65 }
+    ];
+
+    var grid = new Grid({
+        columns: {
+            first: "First Name",
+            last: "Last Name",
+            age: "Age"
+        }
+    }, "dgrid");
+    console.log(grid);
+    grid.renderArray(data);
+
+    
+
+    //change basemap tool
     on(dom.byId("changeBasemap"), "click", function (e) {
         if (domStyle.get(dom.byId("basemapGalleryContainer"), "display") === "block") {
 
@@ -161,13 +142,67 @@ function (
         map: map
     }, "basemapGallery");
 
-    var geocoder = new Geocoder({
-        arcgisGeocoder: {
-            placeholder: "find address"
-        },
-        autoComplete: true,
-        map: map,
-    }, 'search');
-    geocoder.startup();
+    //click 'find conflicts' tool
+    on(dom.byId("findConflicts"), "click", function (e) {
+        dijit.byId("conflictsDialog").show();
+    });
+    
+    //drop down inputs: forOrg, forLayer, inOrd, inLayer
+    var dropDownData = new Memory({
+        data: [
+            { name: "Alabama", id: "AL" },
+            { name: "Alaska", id: "AK" },
+            { name: "American Samoa", id: "AS" },
+            { name: "Arizona", id: "AZ" },
+            { name: "Arkansas", id: "AR" },
+            { name: "Armed Forces Europe", id: "AE" },
+            { name: "Armed Forces Pacific", id: "AP" },
+            { name: "Armed Forces the Americas", id: "AA" },
+            { name: "California", id: "CA" },
+            { name: "Colorado", id: "CO" },
+            { name: "Connecticut", id: "CT" },
+            { name: "Delaware", id: "DE" }
+        ]
+    });
+
+    var forOrgCB = new ComboBox({
+        id: "forOrg",
+        name: "state",
+        value: "MC DOT",
+        store: dropDownData,
+        searchAttr: "name"
+    }, "forOrg");
+
+    var forLayerCB = new ComboBox({
+        id: "forLayer",
+        name: "state",
+        value: "Layer",
+        store: dropDownData,
+        searchAttr: "name"
+    }, "forLayer");
+
+    var inOrgCB = new ComboBox({
+        id: "inOrg",
+        name: "state",
+        value: "PGC Dot",
+        store: dropDownData,
+        searchAttr: "name"
+    }, "inOrg");
+
+    var inLayerCB = new ComboBox({
+        id: "inLayer",
+        name: "state",
+        value: "Layer",
+        store: dropDownData,
+        searchAttr: "name"
+    }, "inLayer");
+
+    var cancelBtn = new Button({
+        label: "Close",
+        onClick: function () {
+            console.log('clicked');
+            dijit.byId("conflictsDialog").hide();
+        }
+    }, "cancel");   
 
 });
